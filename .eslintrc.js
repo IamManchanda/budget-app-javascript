e307463da96ec2fa481f089973c03e78dcf32a1e
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-04-29 06:47:13 +0530

    More work on budget app!

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'no-var': 0,
+    'one-var': 0,
+    'one-var-declaration-per-line': 0,
+    'no-trailing-spaces': 0,
+    'no-unused-expressions': 0,
+    'arrow-body-style': 0,
+    'no-shadow': 0,
+    'no-param-reassign': 0,
+    'func-names': 0,
+    'no-alert': 0,
+    'no-console': 0
+  },
+};
---
+++ js/app.js
@@ -0,0 +1,190 @@
+/**
+ * Budget App JavaScript
+ */
+
+// Helper functions for grabbing multiple elements
+const getDestructuredElementsByIds = (document) => {
+  return new Proxy({}, {
+    get: (_, id) => document.getElementById(id),
+  });
+};
+
+const { 
+  addType, 
+  addDescription, 
+  addValue, 
+  addButton,
+  incomeList,
+  expensesList,
+} = getDestructuredElementsByIds(document);
+
+const BudgetController = (() => {
+  class Expense {
+    constructor(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+  }
+
+  class Income {
+    constructor(id, description, value) {
+      this.id = id;
+      this.description = description;
+      this.value = value;
+    }
+  }
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+    budget: 0,
+    expensePercentage: -1,
+  };
+
+  const calculateTotal = (type) => {
+    let sum = 0;
+    _.forEach(data.allItems[type], (currentItem) => {
+      sum += currentItem.value;
+    });
+    data.totals[type] = sum;
+  };
+
+  return {
+    addItem(type, description, value) {
+      let newItem, id;
+
+      if (data.allItems[type].length > 0) {
+        id = data.allItems[type][data.allItems[type].length - 1];
+      } else {
+        id = 0;
+      }
+
+      if (type === 'exp') {
+        newItem = new Expense(id, description, value);
+      } else if (type === 'inc') {
+        newItem = new Income(id, description, value);
+      }
+
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+    calculateBudget() {
+      calculateTotal('exp');
+      calculateTotal('inc');
+      data.budget = data.totals.inc - data.totals.exp;
+
+      if (data.totals.inc > 0) {
+        data.expensePercentage = Math.round((data.totals.exp / data.totals.inc) * 100); 
+      } else {
+        data.expensePercentage = -1;
+      }
+    },
+    getBudget() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        expensePercentage: data.expensePercentage,
+      };
+    },
+  };
+})();
+
+const UIController = (() => {
+  return {
+    getInput() {
+      return { 
+        type: addType.value, 
+        description: addDescription.value, 
+        value: parseFloat(addValue.value),
+      };
+    },
+    addListItem(obj, type) {
+      let grabbedDomContent, grabbedDomElement;
+
+      if (type === 'inc') {
+        grabbedDomContent = `
+          <div class="item clearfix" id="income-${obj.id}">
+            <div class="item__description">${obj.description}</div>
+            <div class="right clearfix">
+              <div class="item__value">${obj.value}</div>
+              <div class="item__delete">
+                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+              </div>
+            </div>
+          </div>
+        `;
+        grabbedDomElement = incomeList;
+      } else if (type === 'exp') {
+        grabbedDomContent = `
+          <div class="item clearfix" id="expense-${obj.id}">
+            <div class="item__description">${obj.description}</div>
+            <div class="right clearfix">
+              <div class="item__value">- ${obj.value}</div>
+              <div class="item__percentage">21%</div>
+              <div class="item__delete">
+                <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+              </div>
+            </div>
+          </div>
+        `;
+        grabbedDomElement = expensesList;
+      }
+
+      grabbedDomElement.insertAdjacentHTML('beforeend', grabbedDomContent);
+    },
+    clearFields() {
+      const fields = [addDescription, addValue];
+      _.forEach(fields, (field) => {
+        field.value = '';
+      });
+      fields[0].focus();
+    },
+  };
+})();
+
+const AppController = ((BudgetController, UIController) => {
+  const updateBudget = () => {
+    BudgetController.calculateBudget();
+    const budget = BudgetController.getBudget();
+    console.log(budget);
+  };
+
+  const addItemController = () => {
+    const input = UIController.getInput();
+    
+    if (input.description !== '' && !this.isNaN(input.value) && input.value !== 0) {
+      const newItem = BudgetController.addItem(input.type, input.description, input.value);
+      UIController.addListItem(newItem, input.type);
+      UIController.clearFields();
+      updateBudget();
+    }
+  };
+
+  const setupEventListeners = () => {
+    addButton.addEventListener('click', addItemController);
+    document.addEventListener('keypress', (event) => {
+      const charCode = event.which ? event.which : event.keyCode;
+      
+      if (charCode === 13) { 
+        addItemController();
+      }
+    });
+  };
+
+  return {
+    init() {
+      setupEventListeners();
+    },
+  };
+})(BudgetController, UIController);
+
+AppController.init();
+
